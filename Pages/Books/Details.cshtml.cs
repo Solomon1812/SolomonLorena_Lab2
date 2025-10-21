d0854fe1e015ba95608864ce5cb85a9c51d202c1
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-10-21 11:11:21 +0300

    MERGE TOT

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -28,8 +28,7 @@ namespace Solomon_Lorena_Lab2.Pages.Books
                 return NotFound();
             }
 
-            //var book = await _context.Book.FirstOrDefaultAsync(m => m.Id == id);
-
+            
             var book = await _context.Book
                 .Include(b => b.Author)
                 .Include(b => b.Publisher)
