b09199e7d7120e1dc52d5721c7e9016616072434
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-10-28 11:51:36 +0200

    facut lab4 cu tema - working

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,12 @@
+﻿using Solomon_Lorena_Lab2.Models;
+namespace Solomon_Lorena_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Solomon_Lorena_Lab2.Data;
 using Solomon_Lorena_Lab2.Models;
+using Solomon_Lorena_Lab2.ViewModels;
+
 
 namespace Solomon_Lorena_Lab2.Pages.Publishers
 {
@@ -21,9 +23,30 @@ namespace Solomon_Lorena_Lab2.Pages.Publishers
 
         public IList<Publisher> Publisher { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Publisher = await _context.Publisher.ToListAsync();
+            //Publisher = await _context.Publisher.ToListAsync();
+
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+            .Include(i => i.Books)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.PublisherName)
+            .ToListAsync();
+
+            if (id != null)
+            {
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                .Where(i => i.ID == id.Value).Single(); 
+                //where returneaza colectie; single = filtrare un singur publisher
+                PublisherData.Books = publisher.Books;
+            }
+
         }
     }
 }
