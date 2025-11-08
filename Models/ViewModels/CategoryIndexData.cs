53f90c4c68d716cc13c9892c79b3aa783a64c154
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-11-08 12:26:59 +0200

    merge selectarea cartilor dupa categorie - tema facuta

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Solomon_Lorena_Lab2.Models;
+namespace Solomon_Lorena_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
