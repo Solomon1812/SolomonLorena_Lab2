d29b088bb074ebf7c1bbb31171efbce27b531629
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-11-08 13:13:59 +0200

    dupa scaffold

--- Data/Solomon_Lorena_Lab2Context.cs
+++ Data/Solomon_Lorena_Lab2Context.cs
@@ -18,5 +18,6 @@ namespace Solomon_Lorena_Lab2.Data
         public DbSet<Solomon_Lorena_Lab2.Models.Publisher> Publisher { get; set; } = default!;
         public DbSet<Solomon_Lorena_Lab2.Models.Author> Author { get; set; } = default!;
         public DbSet<Solomon_Lorena_Lab2.Models.Category> Category { get; set; } = default!;
+        public DbSet<Solomon_Lorena_Lab2.Models.Member> Member { get; set; } = default!;
     }
 }
---
+++ Pages/Members/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Solomon_Lorena_Lab2.Data;
+using Solomon_Lorena_Lab2.Models;
+
+namespace Solomon_Lorena_Lab2.Pages.Members
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Solomon_Lorena_Lab2.Data.Solomon_Lorena_Lab2Context _context;
+
+        public DetailsModel(Solomon_Lorena_Lab2.Data.Solomon_Lorena_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Member Member { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member.FirstOrDefaultAsync(m => m.ID == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Member = member;
+            }
+            return Page();
+        }
+    }
+}
