15c8fc044b625945aea46082e287621a7ffb2932
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-11-08 13:38:35 +0200

    facut pasul 16 de modificare a program cs succes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Solomon_Lorena_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Solomon_Lorena_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Solomon_Lorena_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Solomon_Lorena_Lab2Context' not found.")));
+
+
+// 1. Register the new Identity Context (from Lab 5, Step 16)
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Solomon_Lorena_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Solomon_Lorena_Lab2Context' not found.")));
+
+// 2. Configure Identity to use the new Context (from Lab 5, Step 16)
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
+
+
 
 var app = builder.Build();
 
