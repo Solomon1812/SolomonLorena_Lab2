c88a0a211ad106d4be7439d7c47d7a1e80e730a6
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-11-08 11:07:13 +0200

    pana la pas 5

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Solomon_Lorena_Lab2.Models;
+namespace Solomon_Lorena_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+         public IEnumerable<Publisher> Publishers { get; set; }
+         public IEnumerable<Book> Books { get; set; }
+    }
+}
