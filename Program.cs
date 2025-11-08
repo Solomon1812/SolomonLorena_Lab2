5839dff61f52121c0dcda9e677dae5ec5cee7b85
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-11-08 14:55:08 +0200

    works partially registration and login work members saved net users not saved

--- Program.cs
+++ Program.cs
@@ -16,7 +16,17 @@ builder.Services.AddDbContext<LibraryIdentityContext>(options =>
 
 // 2. Configure Identity to use the new Context (from Lab 5, Step 16)
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-    options.SignIn.RequireConfirmedAccount = true)
+{
+    // Temporarily disable confirmation for testing registration flow
+    options.SignIn.RequireConfirmedAccount = false;
+
+    // --- RELAXED PASSWORD REQUIREMENTS (Temporary for testing) ---
+    options.Password.RequireDigit = false;          // No number required
+    options.Password.RequireLowercase = false;      // No lowercase required
+    options.Password.RequireUppercase = false;      // No uppercase required
+    options.Password.RequireNonAlphanumeric = false; // No special character required
+    options.Password.RequiredLength = 6;            // Minimum length of 6 characters
+})
     .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 
