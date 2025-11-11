f8265c0914cac457c6bbdc6a543518620248749c
Solomon1812 <solomonlorenasibiu@gmail.com>
2025-11-11 10:16:05 +0200

    pana la pas10, adaugat user lorena.solomon@gmail.com pass Lorena2004$ si admin@gmail.com pass Admin2004$

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -91,6 +91,7 @@ namespace Solomon_Lorena_Lab2.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -100,6 +101,7 @@ namespace Solomon_Lorena_Lab2.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
+             
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
